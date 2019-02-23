e1217d2c81e875df196802a54400c84b28e1821a
dal9 <kig0522@gmail.com>
2019-02-24 03:00:54 +0900

    리스트 1-7 독립시킨 DB 연결 기능인 SimpleConnectionMaker

---
+++ src/main/java/springbook/user/dao/SimpleConnectionMaker.java
@@ -0,0 +1,17 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {
+
+    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/springbook", "root", "qwer1234"
+        );
+        return c;
+    }
+
+}
