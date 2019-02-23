aef3889d7eb2b5ca6a6ecf1003a13acbd9fb1d2c
dal9 <kig0522@gmail.com>
2019-02-24 02:40:10 +0900

    리스트 1-4 getConnection() 메소드를 추출해서 중복을 제거한 UserDao

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -7,11 +7,7 @@ import java.sql.*;
 public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-
-        Class.forName("org.mariadb.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mariadb://localhost:3306/springbook", "root", "qwer1234"
-        );
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values (?, ?, ?)"
         );
@@ -27,10 +23,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.mariadb.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mariadb://localhost:3306/springbook", "root", "qwer1234"
-        );
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?"
         );
@@ -50,5 +43,13 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/springbook", "root", "qwer1234"
+        );
+        return c;
     }
 }
