fa5651340478d279098155f43206a43ef54f0cba
dal9 <kig0522@gmail.com>
2019-02-24 02:35:46 +0900

    리스트 1-2 JDBC를 이용한 등록과 조회 기능이 있는 UserDao 클래스

---
+++ src/main/java/springbook/user/domain/User.java
@@ -0,0 +1,14 @@
+package springbook.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    String id;
+    String name;
+    String password;
+
+}
