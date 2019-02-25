d775cad6efb11a1e64d700e059bf1dc4c6f87ba5
dal9 <kig0522@gmail.com>
2019-02-25 18:39:49 +0900

    리스트 1-43 DataSource 타입의 dataSource 빈 정의 메소드

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -3,6 +3,9 @@ package springbook.user.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DaoFactory {
@@ -22,6 +25,17 @@ public class DaoFactory {
     @Bean
     public ConnectionMaker connectionMaker() {
         return new CountingConnectionMaker(realConnectionMaker());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://35.189.131.79:3306/springbook");
+        dataSource.setUsername("root");
+        dataSource.setPassword("qwer1234");
+
+        return dataSource;
     }
 
 }
