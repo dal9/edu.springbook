b99b5bcf43bf2392c7bddcece7f537c5a9fabd9f
dal9 <kig0522@gmail.com>
2019-02-25 17:01:08 +0900

    리스트 1-40 XML 설정정보를 담은 applicationContext.xml

--- src/main/java/UserDaoTest.java
+++ src/main/java/UserDaoTest.java
@@ -1,7 +1,6 @@
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.dao.CountingConnectionMaker;
-import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
@@ -11,7 +10,9 @@ public class UserDaoTest {
 
     public static void main(String[] arg) throws ClassNotFoundException, SQLException {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        //ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
