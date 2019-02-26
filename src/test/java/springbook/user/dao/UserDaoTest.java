a67c84b61e1f4abd513f55dc5b1e85d6dd39a6b3
dal9 <kig0522@gmail.com>
2019-02-26 12:10:33 +0900

    리스트 2-5 JUnit을 적용한 UserDaoTest

---
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,32 @@
+package springbook.user.dao;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UserDaoTest {
+
+    @Test
+    public void addAndGet() throws SQLException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user = new User();
+        user.setId("gyumee");
+        user.setName("박상철");
+        user.setPassword("springno1");
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+    }
+}
