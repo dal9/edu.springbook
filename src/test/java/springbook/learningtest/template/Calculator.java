48a4b0bcec30926a3846cceaee53f5c77189abe6
dal9 <kig0522@gmail.com>
2019-03-20 23:53:03 +0900

    리스트 3-32 try/catch/finally를 적용한 calcSum() 메소드

--- src/test/java/springbook/learningtest/template/Calculator.java
+++ src/test/java/springbook/learningtest/template/Calculator.java
@@ -6,14 +6,29 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filepath));
-        Integer sum = 0;
-        String line = null;
-        while((line = br.readLine()) != null) {
-            sum += Integer.valueOf(line);
-        }
 
-        br.close();
-        return sum;
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+
+            Integer sum = 0;
+            String line = null;
+            while((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+            return sum;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 }
