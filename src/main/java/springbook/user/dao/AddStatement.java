6e8b8abd70cc624a1ee22b0de1337d02108b96b4
dal9 <kig0522@gmail.com>
2019-03-20 17:14:23 +0900

    리스트 3-14 User 정보를 생성자로부터 제공받도록 만든 AddStatement

--- src/main/java/springbook/user/dao/AddStatement.java
+++ src/main/java/springbook/user/dao/AddStatement.java
@@ -7,6 +7,12 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AddStatement implements StatementStrategy {
+    User user;
+
+    public AddStatement(User user) {
+        this.user = user;
+    }
+
     public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?, ?, ?)");
         ps.setString(1, user.getId());
