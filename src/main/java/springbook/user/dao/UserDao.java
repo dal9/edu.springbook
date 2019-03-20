4ca7c5f24dc068530926beba7be8f470941f87e5
dal9 <kig0522@gmail.com>
2019-03-20 16:44:23 +0900

    리스트 3-6 변하는 부분을 메소드로 추출한 후의 deleteAll()

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -36,7 +36,7 @@ public class UserDao {
 
         try {
             c = dataSource.getConnection();
-            ps = c.prepareStatement("delete from users");
+            ps = makeStatement(c);
             ps.executeUpdate();
         } catch (SQLException e) {
             throw e;
@@ -121,5 +121,11 @@ public class UserDao {
                 }
             }
         }
+    }
+
+    private PreparedStatement makeStatement(Connection c) throws SQLException {
+        PreparedStatement ps;
+        ps = c.prepareStatement("delete from users");
+        return ps;
     }
 }
